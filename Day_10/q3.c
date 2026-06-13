57ea07fcbcdcaa35952a2f2085945fbbc4f72c49
Akriti_Pandey_05 <pandeyakriti0513@gmail.com>
2026-06-13 18:31:06 +0530

    Restore Day_10 files

---
+++ Day_10/q3.c
@@ -0,0 +1,23 @@
+//Q. Write a program to print number pyramid//
+#include<stdio.h>
+int main(){
+    int i,j,n;
+    printf("Enter the number of rows:\n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n-i;j++){
+            printf(" ");
+        }
+        for(j=1;j<=i;j++){
+            printf("%d",j);
+        }
+        for(j=i-1;j>=1;j--){
+            if(j==0){
+                printf(" ");
+            }
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
